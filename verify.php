c0e15f9e3a6c5c47fa8f84b07725bf54b7104562
Khuong Nguyen <khuong.nguyen.duy@gosell.vn>
2024-04-20 15:54:21 +0700

    add recaptcha

--- verify.php
+++ verify.php
@@ -6,8 +6,27 @@ if (isset($_POST) && isset($_POST['g-token'])) {
     $token = $_POST['g-token'];
     $ip = $_SERVER['REMOTE_ADDR'];
 
-    $url = "https://www.google.com/recaptcha/api/siteverify?secret=".$secretKey."&response=".$token."&remoteip=".$ip;
-    $request = file_get_contents($url);
+
+
+    $post_data = http_build_query(
+                    array(
+                        'secret' => $secretKey,
+                        'response' => $_POST['g-recaptcha-response'],
+                        'remoteip' => $_SERVER['REMOTE_ADDR']
+                    )
+                );
+
+    $opts = array('http' =>
+                array(
+                    'method'  => 'POST',
+                    'header'  => 'Content-type: application/x-www-form-urlencoded',
+                    'content' => $post_data
+                )
+            );
+    $context  = stream_context_create($opts);
+
+    $request = file_get_contents('https://www.google.com/recaptcha/api/siteverify', false, $context);
+
     $response = json_decode($request); 
 
     if ($response->success && $response->action == 'submit' && $response->score >= 0.5) {
