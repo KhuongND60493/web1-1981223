d7b5374d1ef4e8e70c77fadce2d4bd48429fcbc6
Khuong Nguyen <khuong.nguyen.duy@gosell.vn>
2024-04-20 16:01:12 +0700

    add recaptcha

--- verify.php
+++ verify.php
@@ -4,9 +4,9 @@ $_POST = json_decode(file_get_contents('php://input'), true);
 if (isset($_POST) && isset($_POST['g-token'])) {
     $secretKey = '6LeaE8EpAAAAALu563lf_wOxymyDtAS0H5E4x0mx';
     $token = $_POST['g-token'];
-    // $ip = $_SERVER['REMOTE_ADDR'];
-
-
+    if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
+        $_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+    }
 
     $post_data = http_build_query(
                     array(
